2c6add3accc740efa5b8b7906fd52afd12b1c711
Evgeni <Kremeni@gmail.com>
2022-09-15 08:08:14 +0300

    Initial commit

---
+++ HomeWork_9/src/main/java/ru/kremenia/HomeWork9Application.java
@@ -0,0 +1,11 @@
+package ru.kremenia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWork9Application {
+    public static void main(String[] args) {
+        SpringApplication.run(HomeWork9Application.class, args);
+    }
+}
